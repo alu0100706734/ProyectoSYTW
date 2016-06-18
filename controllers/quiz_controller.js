f092c53fffc5af7066a31da3a15b8ced109d03d1
Juan Nicolas <juannicolasvargasmoreno@hotmail.com>
2016-06-18 17:07:34 +0100

    editar preguntas creado

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -55,11 +55,31 @@ exports.create = function(req,res) {
 	});
 };
 
+exports.edit = function(req,res){
+	var quiz = req.quiz;
+	res.render('quizes/edit', {quiz: quiz, errors: []});
+};
+
+exports.update = function(req,res) {
+	req.quiz.pregunta = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+
+	req.quiz.validate().then(function(err){
+		if (err){
+			res. render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+		}
+		else {
+			req.quiz.save({ fields: ["pregunta", "respuesta"]})
+				.then( function(){ res.redirect('/quizes');});
+		}
+	});
+};
+
 
 
 exports.question = function (req, res){
 	models.Quiz.findAll().then(function(quiz){
-	res.render('quizes/show', {quiz: quiz[0]});
+	res.render('quizes/show', {quiz: quiz[0], errors: []});
 	})
 };
 
